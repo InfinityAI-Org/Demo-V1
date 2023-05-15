fd072a952fe4d5d6208f872e1e131c0ff31e02e0
TonyynoT <tonynguyen0403@gmail.com>
2023-05-15 12:26:27 +0930

    initial commit

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,9 @@
-import ai from "./ai.png";
-import fileIcon from "./file.png";
-import swatch from "./swatch.png";
+import ai from "./ai.jpeg";
+import fileIcon from "./file.jpeg";
+import swatch from "./colorpicker.jpeg";
 import download from "./download.png";
 
-import logoShirt from "./logo-tshirt.png";
-import stylishShirt from "./stylish-tshirt.png";
+import logoShirt from "./logoicon.jpeg";
+import stylishShirt from "./shirticon.jpeg";
 
 export { ai, fileIcon, swatch, download, logoShirt, stylishShirt };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,16 +19,16 @@ const Home = () => {
         <motion.section className="home" {...slideAnimation('left')}>
           <motion.header {...slideAnimation("down")}>
             <img 
-              src='./threejs.png'
+              src='./logo.png'
               alt="logo"
-              className="w-8 h-8 object-contain"
+              className="w-12 h-12 object-contain"
             />
           </motion.header>
 
           <motion.div className="home-content" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
-                LET'S <br className="xl:block hidden" /> DO IT.
+                Infinity  AI 
               </h1>
             </motion.div>
             <motion.div
@@ -36,12 +36,12 @@ const Home = () => {
               className="flex flex-col gap-5"
             >
               <p className="max-w-md font-normal text-gray-600 text-base">
-              Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
+              Create your unique and exclusive design with our brand-new AI powered 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
               </p>
 
               <CustomButton 
                 type="filled"
-                title="Customize It"
+                title="lets do it"
                 handleClick={() => state.intro = false}
                 customStyles="w-fit px-4 py-2.5 font-bold text-sm"
               />
